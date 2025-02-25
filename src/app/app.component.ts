baf2652b0f44ffb177c76efea1d46df2b23d30d8
keannoregino <keanno.regino@radenta.com>
2025-02-25 14:14:38 +0800

    added "toast"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { ButtonModule } from 'primeng/button'
+import { ToastModule } from 'primeng/toast'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
