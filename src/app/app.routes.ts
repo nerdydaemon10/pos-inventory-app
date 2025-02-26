09cefe1b1b612e031a7a60dd835b9d9c99d17003
keannoregino <keanno.regino@radenta.com>
2025-02-26 11:56:26 +0800

    added route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './auth/auth.routes';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { GuestGuard } from './auth/guest.guard';
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: "",
+    canActivate: [GuestGuard],
     children: authRoutes
   },
   {
     path: "dashboard",
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
     component: DashboardComponent
   }
 ];
