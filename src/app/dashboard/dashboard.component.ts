099c5593ae8e262f2047cb4a806138c77e156d45
keannoregino <keanno.regino@radenta.com>
2025-02-26 17:44:37 +0800

    initial commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+import { AuthService } from '../auth/auth.service';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'pos-dashboard',
-  imports: [],
+  imports: [ButtonModule, RouterOutlet, SidebarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  constructor(private service: AuthService) {}
 }
