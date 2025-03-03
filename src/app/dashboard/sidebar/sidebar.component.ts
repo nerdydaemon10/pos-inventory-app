a5549fbc504c6c3395e6f11511cef4487219d43b
keannoregino <keanno.regino@radenta.com>
2025-03-03 09:51:11 +0800

    implement "dark mode" dropdown activation

--- src/app/dashboard/sidebar/sidebar.component.ts
+++ src/app/dashboard/sidebar/sidebar.component.ts
@@ -1,26 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NgScrollbar } from 'ngx-scrollbar';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
-
-import { sidebarMenuItems } from './sidebar-menu-items.data';
-import { SidebarDropdownMenuComponent } from './sidebar-dropdown-menu/sidebar-dropdown-menu.component';
+import { SidebarStore } from './sidebar.store';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import _ from 'lodash';
+import { AppStore } from '../../app.store';
+import { SidebarMenuComponent } from "./sidebar-menu/sidebar-menu.component";
+import { filter } from 'rxjs';
+import { SidebarMenu } from './sidebar-menu/sidebar-menu.type';
 
 @Component({
   selector: 'app-sidebar',
   imports: [
     NgScrollbar,
-    ButtonModule, 
+    ButtonModule,
     CommonModule,
     ScrollPanelModule,
-    SidebarDropdownMenuComponent
-  ],
+    SidebarMenuComponent
+],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
-  protected get sidebarMenuItems() {
-    return sidebarMenuItems
+export class SidebarComponent implements OnInit {
+  protected readonly app = inject(AppStore)
+  protected readonly sidebar = inject(SidebarStore)
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.sidebar.init(this.router.url)
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.sidebar.init(event.url)
+    })
+  }
+  protected toggle(menu: SidebarMenu): void {
+    this.sidebar.toggle(menu)
+  }
+  protected navigate(route: string): void {
+    this.router.navigate([route], { relativeTo: this.route})
   }
 }
