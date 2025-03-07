8baf9d18b69f87ec6386bd4be4bf712ce2aae6e6
keannoregino <keanno.regino@radenta.com>
2025-03-07 10:29:58 +0800

    changed "toggle" to "darkModeToggle" and added logout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from "./header/header.component";
 import { AppStore } from '../app.store';
+import { AuthStore } from '../auth/auth.store';
 
 @Component({
   selector: 'pos-dashboard',
@@ -13,6 +14,13 @@ import { AppStore } from '../app.store';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  protected readonly store = inject(AppStore)
-  constructor() {}
+  protected readonly app = inject(AppStore)
+  protected readonly auth = inject(AuthStore)
+
+  constructor(private router: Router) {}
+  
+  protected logout(): void {
+    this.auth.logout()
+    this.router.navigate([''])
+  }
 }
