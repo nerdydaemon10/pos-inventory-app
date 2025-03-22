11cc0aa6ccdee00eb67d87dd1dca07e0aa7d99f9
keannoregino <keanno.regino@radenta.com>
2025-03-22 11:07:43 +0800

    initial commit

---
+++ src/app/inventory/products/product-create/product-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pos-product-create',
+  imports: [],
+  templateUrl: './product-create.component.html',
+  styleUrl: './product-create.component.css'
+})
+export class ProductCreateComponent {
+
+}
--- src/app/inventory/products/products-list/products-list.component.ts
+++ src/app/inventory/products/products-list/products-list.component.ts
@@ -1,13 +1,121 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { Product } from '../product.model';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'pos-products-list',
-  imports: [TableModule, ButtonModule],
+  imports: [TableModule, ButtonModule, RouterLink, BadgeModule],
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
 export class ProductsListComponent {
-  
+  protected products: Product[] = [
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    },
+    {
+      id: 1,
+      sku: "KBD-K12",
+      code: "123456789",
+      name: "Keychron K12",
+      brand: "Keychron",
+      category: "Keyboard"
+    }
+  ]
 }
