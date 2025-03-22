87ca748dfebae62efc4f8a23736c8f24f97e5c9e
keannoregino <keanno.regino@radenta.com>
2025-03-22 11:11:29 +0800

    added 'sanitize, last, and includes' methods

--- src/app/core/helpers/route.helper.ts
+++ src/app/core/helpers/route.helper.ts
@@ -1,7 +1,26 @@
 import _ from "lodash"
 
 export class RouteHelper {
-  public static transformUrl(url: string): string {
-    return url.replace("/dashboard", "").replace(/^\/*/, '')
+  public static sanitize(url: string): string {
+    return url.replace("/dashboard", "").replace(/^\/*/, "")
+  }
+  
+  public static last(route?: string): string | undefined {
+    if (_.isNil(route)) {
+      return undefined
+    }
+
+    return _.last(route.split('/'))
+  }
+  public static includes(routeA: string, routeB?: string): boolean {
+    const routes = routeA.split("/")
+    
+    for (let i = 0; i < routes.length; i++) {
+      if (routes[i] === routeB) {
+        return true
+      }
+    }
+
+    return false
   }
 }
