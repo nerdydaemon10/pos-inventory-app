d2aa05a0512ebe8f9d0ad3c26c532f6d8a4fae43
keannoregino <keanno.regino@radenta.com>
2025-03-22 11:07:10 +0800

    added 'products/create'

--- src/app/inventory/inventory.routes.ts
+++ src/app/inventory/inventory.routes.ts
@@ -4,5 +4,9 @@ export const InventoryRoutes: Routes = [
   {
     path: "products",
     loadComponent: () => import("./products/products-list/products-list.component").then(c => c.ProductsListComponent)
+  },
+  {
+    path: "products/create",
+    loadComponent: () => import("./products/product-create/product-create.component").then(c => c.ProductCreateComponent)
   }
 ]
